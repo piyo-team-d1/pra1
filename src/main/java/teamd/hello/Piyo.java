e3818a0dd17b8cb481cf93468cefd6c7d7045c7d
m-nakajo <MarikoA_Nakajo@niandc.co.jp>
2018-01-24 13:01:19 +0900

    add junit test sample

---
+++ src/main/java/teamd/hello/Piyo.java
@@ -0,0 +1,13 @@
+package teamd.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class Piyo 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
