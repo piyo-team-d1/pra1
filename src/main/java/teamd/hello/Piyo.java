5bf6ee0038b160a92e2c2ea2905597312bd75938
m-nakajo <MarikoA_Nakajo@niandc.co.jp>
2018-02-01 10:18:35 +0900

    delete !

--- src/main/java/teamd/hello/Piyo.java
+++ src/main/java/teamd/hello/Piyo.java
@@ -14,7 +14,7 @@ public class Piyo
 	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World" );
     }
 	
 	public String getPassword(String userId) {
