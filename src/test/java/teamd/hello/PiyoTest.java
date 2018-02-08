e000d65f1678025cdf908a86aee950732a39e017
Hiroya Miyanoiri <hiroya_miyanoiri@niandc.co.jp>
2018-02-08 10:31:42 +0900

    test commit

--- src/test/java/teamd/hello/PiyoTest.java
+++ src/test/java/teamd/hello/PiyoTest.java
@@ -34,7 +34,7 @@ public class PiyoTest
     public void testPiyo()
     {
         assertTrue( true );
-    	Piyo piyo = new Piyo();
+    	Piyo piiyo = new Piyo();
         assertEquals("piyo",piyo.getPassword("hoge"));
         assertFalse("hoyo".equals(piyo.getPassword("hoyo")));
     }
